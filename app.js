dddb218ababef2c2a2277c81156979141250560a
1kzpro <kazymizam@mail.ru>
2020-08-19 22:34:50 -0500

    update

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const nocache = require('nocache');
 
 var indexRouter = require('./routes/index');
 
@@ -16,6 +17,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(nocache());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
